a21d03e9b197511d60a3adcb8ae84447764c3dc2
Robin Richard Canavaggio <22174078+r-richardcanavaggio@users.noreply.github.com>
2026-08-08 12:39:49 +0200

    ex00 done

---
+++ S1E9.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+class Character(ABC):
+    """Abstract class representing a character"""
+    def __init__(self, first_name, is_alive=True):
+            """Constructor for Character"""
+            self.first_name = first_name
+            self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self):
+        """Method to change health state of the charater. Should be implemented by subclasses"""
+        pass
+
+
+class Stark(Character):
+    """Class representing a Stark character"""
+    def die(self):
+        """Overrides abstract method from the base class and updates is_alive to False"""
+        self.is_alive = False
